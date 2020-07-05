a7953a022d69b6c7916c1d133e61502cc7f02a1c
gearuprepo <sb.desikan@gmail.com>
2020-07-06 00:19:10 +0530

    Updated JUnit

---
+++ openalgos/src/test/java/org/MultiplyTest.java
@@ -0,0 +1,23 @@
+package org;
+
+import static org.junit.Assert.assertTrue;
+import org.Multiply;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MultiplyTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testNaiveImpl()
+    {
+        assertTrue( Multiply.naivemultiply("55", "34") == (55*34) );
+        assertTrue( Multiply.naivemultiply("554", "3") == (554*3) );
+        assertTrue( Multiply.naivemultiply("552", "344") == (552*344) );
+        assertTrue( Multiply.naivemultiply("1", "1") == (1*1) );
+    }
+}

14f51bc5631684070675dec0982a1e30997f0ea8
Aravind B <aravindb@Aravinds-MacBook-Pro.local>
2020-09-28 00:01:58 +0530

    Updated Code

--- openalgos/src/main/java/org/sort/qsort/QSort.java
+++ openalgos/src/main/java/org/sort/qsort/QSort.java
@@ -12,38 +12,36 @@ import java.util.List;
 
 public class QSort {
 
-    public int[] qSort(int[] input, int min, int max){
-        int pivot = input[min];
-        int i = min,iter = min+1;
-
-        if(max-min <= 1){
-            return null;
-        }
-        for (;iter<max;iter++){
-            int elem = input[iter];
-            if(elem  < pivot){
-                if ((iter - i)>1){
-                    int temp = input[i+1];
-                    input[i+1]=input[iter];
-                    input[iter]=temp;
-                }
-                i++;
+    public int qSortFirst(int[] input, int start, int end){
+        int pivot = input[start];
+        int pivotmarker = start;
+        for(int i = start+1;i<=end;i++){
+            if(input[i]<pivot) {
+                int temp = input[i];
+                input[i] = input[pivotmarker + 1];
+                input[pivotmarker + 1] = temp;
+                pivotmarker++;
             }
         }
-        input[min] = input[i];
-        input[i] = pivot;
-        if((i-1)-min > 1) qSort(input,min,i-1);
-        if(max - (i+1) >1) qSort(input,i+1, max);
-        return null;
+        int temp = input[pivotmarker];
+        input[pivotmarker] = pivot;
+        input[start] = temp;
+
+        if((pivotmarker-1)> start) qSortFirst(input,start, pivotmarker-1);
+        if(end> (pivotmarker+1)) qSortFirst(input,pivotmarker+1,end);
+
+    return 0;
     }
+
+
     public static void main(String[] args) throws IOException {
         QSort qs = new QSort();
-        //int[] inpl = {5,3,6,7,2,8};
+        //int[] inpl = {5,3,6,7,2,8,12,44,1,0,500,43,1234,523,65,77,95,234,656};
         String filename = "./files/QuickSort.txt";
         Charset charset = Charset.forName("ISO-8859-1");
         List<String> result = Files.readAllLines(Paths.get(filename), charset);
         int[] inpl = result.stream().mapToInt(Integer::parseInt).toArray();
-        qs.qSort(inpl,0,inpl.length);
+        qs.qSortFirst(inpl,0,inpl.length-1);
         Arrays.stream(inpl).asLongStream().forEach(System.out::println);
        // Arrays.sort(inpl);
        // Arrays.stream(inpl).asLongStream().forEach(System.out::println);
